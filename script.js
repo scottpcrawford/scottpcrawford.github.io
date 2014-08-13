c019fddf71c65988c05cd3262ebb5d7348aeed44
scottpcrawford <scott@crawhouse.com>
2014-08-13 16:35:18 +0200

    Animated Sidebar Menu

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 var main = function(){
-    $('.icon-menu').click( function(){
-        $('.menu').animate({
-            left: '0px' }, 200);
-    $('body').animate({
-        left: '285px' }, 200);
-    });
-    
-    $('.icon-close').click( function(){
-        $('.menu').animate({
-            left: '-285px'}, 200);
-        $('body').animate({
-            left: '0px'}, 200);
-    });
-    
+    $('.menu-list').hover( 
+    	function(){
+        	$(this).animate({
+            	left: '30px' }, 200)}, 
+        function(){
+        	$(this).animate({
+        		left: '150px'}, 200)}
+       );
+
 };
 
 $(document).ready(main)
