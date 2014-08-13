e6ed02ea140ee67abe19083f35b80ffd136fd2c3
scottpcrawford <scott@crawhouse.com>
2014-08-13 16:42:30 +0200

    Fixed poor layout on smaller window

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var main = function(){
     $('.menu-list').hover( 
     	function(){
         	$(this).animate({
-            	left: '30px' }, 200)}, 
+            	left: '130px' }, 200)}, 
         function(){
         	$(this).animate({
-        		left: '150px'}, 200)}
+        		left: '250px'}, 200)}
        );
 
 };
