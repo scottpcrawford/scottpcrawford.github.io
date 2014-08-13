5ca827817c930a1792c021349128ef3faf671dd4
scottpcrawford <scott@crawhouse.com>
2014-08-13 12:25:27 +0200

    Added menu-list div

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var main = function(){
-
+    $('.icon-menu').click( function(){
+        $('.menu').animate({
+            left: '0px' }, 200);
+    $('body').animate({
+        left: '285px' }, 200);
+    });
+    
+    $('.icon-close').click( function(){
+        $('.menu').animate({
+            left: '-285px'}, 200);
+        $('body').animate({
+            left: '0px'}, 200);
+    });
+    
 };
 
-$(document).ready(main);
+$(document).ready(main)
